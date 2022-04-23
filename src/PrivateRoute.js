87c1d015d6fe0de03feaba74d9aab3b8f757ccb5
sz11u21 <sz11u21@soton.ac.uk>
2022-04-23 23:38:44 +0100

    signin/signup

---
+++ src/PrivateRoute.js
@@ -0,0 +1,9 @@
+// import React from "react"
+// import { Route, Navigate } from "react-router-dom"
+// import { useAuth } from "./AuthContext"
+//
+// const PrivateRoute = ({Component}) => {
+//     const currentUser  = useAuth()
+//
+//     return currentUser ? <Component /> : <Navigate to="/login" />;
+// }
